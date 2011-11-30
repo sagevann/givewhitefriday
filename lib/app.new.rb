2b1d70125f40a5c9d30b1cbed81e33d7ffe7f150
sagevann <svann@whitepages.com>
2011-11-29 17:12:30 -0800

    reverting by hand

---
+++ lib/app.new.rb
@@ -0,0 +1,42 @@
+require 'sinatra'
+require 'data_mapper'
+
+
+
+
+helpers do
+  def parse_donation()
+    #name = rack.request.query_hash["name"]
+   puts params
+   # name = request.query_string.match("name=(.*)").gsub(/%20/,' ')
+    { name: 'name' }
+  end
+end
+
+class GiveWhiteFriday  < Sinatra::Application
+  set :views, settings.root + '/../views'
+
+  get '/' do
+    erb :index
+  end
+  
+  get '/about' do
+    erb :about
+  end
+  
+  get '/callback' do
+    Donations.create( params )
+    erb :callback
+  end
+  
+  get '/data' do
+    @donations = Donations.all
+    erb :data
+  end
+  
+  get '/donate/:name/:amount/:charity' do
+    Donations.create( params )
+    "Successfully added #{params[:name]}'s donation to #{params[:charity]} of #{params[:amount]}"
+  end
+  
+end
