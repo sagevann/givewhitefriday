fd5190abb588e2e389b9b9a0974a533ca27b99bf
sagevann <svann@whitepages.com>
2011-11-29 16:52:16 -0800

    added preliminary database and data

--- lib/app.rb
+++ lib/app.rb
@@ -1,4 +1,31 @@
 require 'sinatra'
+require 'data_mapper'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
+# TODO: Add database models here.
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
+
+class Donations
+  include DataMapper::Resource
+  property :id,           Serial
+  property :name,         String, :required => true
+  property :amount,       String, :required => true
+  property :charity,      String
+  property :completed_at, DateTime
+end
+DataMapper.finalize
+
+
+helpers do
+  def parse_donation()
+    #name = rack.request.query_hash["name"]
+   puts params
+   # name = request.query_string.match("name=(.*)").gsub(/%20/,' ')
+    { name: 'name' }
+  end
+end
 
 class GiveWhiteFriday  < Sinatra::Application
   set :views, settings.root + '/../views'
@@ -8,6 +35,22 @@ class GiveWhiteFriday  < Sinatra::Application
   end
   
   get '/about' do
-      erb :about
+    erb :about
   end
+  
+  get '/callback' do
+    Donations.create( params )
+    erb :callback
+  end
+  
+  get '/data' do
+    @donations = Donations.all
+    erb :data
+  end
+  
+  get '/donate/:name/:amount/:charity' do
+    Donations.create( params )
+    "Successfully added #{params[:name]}'s donation to #{params[:charity]} of #{params[:amount]}"
+  end
+  
 end
